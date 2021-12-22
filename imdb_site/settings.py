9f4f7c2c354f2c6a8ce2bcbce92c34dc6226bc98
egepsihora <egepsihora@gmail.com>
2021-12-22 13:31:07 +0200

    ALLOWED_HOSTS=['pysankanataliia.pythonanywhere.com']

--- imdb_site/settings.py
+++ imdb_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6y^9^1za8btov5eegg)vi&+22g3u6lhji$tk!e_o3!jvm%v*do
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pysankanataliia.pythonanywhere.com']
 
 
 # Application definition
